18d9536b1aa578ceae7860b4b03a48efb3abef7b
Petr <petristomin@gmail.com>
2023-02-06 21:36:54 +0200

    much more working

--- views/Game.jsx
+++ views/Game.jsx
@@ -1,22 +1,19 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function Game({ findAllCards }) {
-  console.log(findAllCards)
+module.exports = function Game({ card }) {
+  // console.log(findAllCards)
 
   // const { findAllCards } = props;
   // console.log(findAllCards)
+  // { const randomId = Math.Floor(Math.random() * findAllCards.length); }
   return (
     <Layout>
-      { 
-
-
-
-
-      }
-
-
-      {findAllCards[0].question}
+      <div className="gameContainer">
+        {card.question}
+        <button id={`${card.id}`} name="yes" type="button">Да</button>
+        <button id={`${card.id}`} name="no" type="button">Нет</button>
+      </div>
       {/* {question, answer} */}
     </Layout>
   );
