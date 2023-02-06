f53aab51c25998e8379f28bac9fa04706d38749a
Petr <petristomin@gmail.com>
2023-02-06 14:08:01 +0200

    3 pages without css

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const { sequelize } = require('./db/models');
 
 const app = express();
+
+const authorization = require('./routers/openAuthorization')
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public/')));
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+app.use('/', authorization)
 
-app.listen(process.env.PORT, () => {
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await sequelize.authenticate();
+  } catch (err) {
+    console.log(err, 'Error!');
+  }
   console.log(`server started on http://localhost:${process.env.PORT}`);
 });
